25bacc129cb3bff3c0b72d2a0a54d28d0c61c1d2
Avinash <134164909+AvinashP-1611@users.noreply.github.com>
2024-01-21 20:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // complete this js code
 // Person constructor function
 function Person(name, age) {
-  this.name = name;
-  this.age = age;
+  this._name = name;
+  this._age = age;
 }
 
 // Add greet() method to Person prototype
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
 };
 
 // Employee constructor function
 function Employee(name, age, jobTitle) {
   // Call the Person constructor to initialize name and age
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
+  Person.call(name,age)
+  this._jobTitle = jobTitle;
 }
 
 // Inherit the Person prototype in the Employee prototype
-Employee.prototype = Object.create(Person.prototype);
+// Employee.prototype = Object.create(Person.prototype);
 
-// Set the constructor property back to Employee
-Employee.prototype.constructor = Employee;
+// // Set the constructor property back to Employee
+// Employee.prototype.constructor = Employee;
 
 // Add jobGreet() method to Employee prototype
 Employee.prototype.jobGreet = function() {
